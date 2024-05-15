c2972b804e232c0e54d4615c730216a7ba758047
JuanFonte <devjuanfonte@gmail.com>
2024-05-15 13:28:20 -0300

    add login page

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -3,10 +3,28 @@ let historiaButton = document.querySelector('#historia');
 let festivalButton = document.querySelector('#festival');
 let headerButtons = document.querySelectorAll('nav-link');
 
+
+const homePage = document.querySelector('.homePage');
+const loginPage = document.querySelector('.loginPage');
+
+function changeToLoginPage(){
+    
+    homePage.style.display = 'none';
+    loginPage.style.display = 'grid';
+    
+}
+
+
+
 homeButton.addEventListener('click', ()=>{
     historiaButton.classList.remove('active')
     festivalButton.classList.remove('active')
-    homeButton.classList.add('active')
+    homeButton.classList.add('active')   
+
+    loginPage.style.display = 'none';
+    homePage.style.display = 'grid';
+
+
 })
 
 historiaButton.addEventListener('click', ()=>{
