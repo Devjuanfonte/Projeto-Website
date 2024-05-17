078d7cef495c6393d8c4065e5e376eba379c3789
Devjuanfonte <devjuanfonte@gmail.com>
2024-05-17 19:01:54 -0300

    Atualizações

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -5,18 +5,27 @@ let headerButtons = document.querySelectorAll('nav-link');
 
 
 const homePage = document.querySelector('.homePage');
+const historiaPage = document.querySelector('.historiaPage');
 const loginPage = document.querySelector('.loginPage');
 const registerPage = document.querySelector('.registerPage');
 const homeNavButton = document.querySelector('#homeButton');
 const body = document.querySelector('body');
 
 function changeToLoginPage(){
-    
+
+    historiaPage.style.display = 'none';
     homePage.style.display = 'none';
     registerPage.style.display = 'none';
     loginPage.style.display = 'grid';
 
+    historiaButton.classList.remove('active')
+    festivalButton.classList.remove('active')
+    homeButton.classList.remove('active')  
+
+   
+
     homeNavButton.addEventListener('click',()=>{
+        historiaPage.style.display = 'none';
         loginPage.style.display = 'none';
         registerPage.style.display = 'none';
         homePage.style.display = 'grid';
@@ -26,10 +35,16 @@ function changeToLoginPage(){
 function changeToRegisterPage(){
     
     homePage.style.display = 'none';
+    historiaPage.style.display = 'none';
     loginPage.style.display = 'none';
     registerPage.style.display = 'grid';
 
+    historiaButton.classList.remove('active')
+    festivalButton.classList.remove('active')
+    homeButton.classList.remove('active')  
+
     homeNavButton.addEventListener('click',()=>{
+        historiaPage.style.display = 'none';
         loginPage.style.display = 'none';
         registerPage.style.display = 'none';
         homePage.style.display = 'grid';
@@ -42,8 +57,9 @@ homeButton.addEventListener('click', ()=>{
     historiaButton.classList.remove('active')
     festivalButton.classList.remove('active')
     registerPage.style.display = 'none';
-    homeButton.classList.add('active')   
+    homeButton.classList.add('active')  
 
+    historiaPage.style.display = 'none';
     loginPage.style.display = 'none';
     homePage.style.display = 'grid';
 
@@ -54,6 +70,12 @@ historiaButton.addEventListener('click', ()=>{
     homeButton.classList.remove('active')
     festivalButton.classList.remove('active')
     historiaButton.classList.add('active')
+
+    loginPage.style.display = 'none';
+    registerPage.style.display = 'none';
+    homePage.style.display = 'none';
+    historiaPage.style.display = 'grid';
+
 })
 festivalButton.addEventListener('click', ()=>{
     homeButton.classList.remove('active')
